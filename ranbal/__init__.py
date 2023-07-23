b5095208cc8d47973f25eebf5795b0c3b10f700f
Adam Masiarek <masiarek@gmail.com>
2023-07-23 09:37:53 -0400

    ver 0.0.2

--- ranbal/__init__.py
+++ ranbal/__init__.py
@@ -1,5 +1,6 @@
 import builtins
 from collections import defaultdict
+import collections  # ?
 from contextlib import contextmanager
 import csv
 from fractions import Fraction
@@ -12,15 +13,11 @@ import random
 import re
 import sys
 import types
-
-##         * maybe just super-regular HTML, with classes and names to make it easy?
-##     * allow displaying in floats
-##     * restore printing the preference matrix (it's lurking in 1.x versions)
-##
+import string
 
 __doc__ = "Create Random Elections and Random Ballots"
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 __all__ = [
     'Method', # class for method metadata
@@ -61,3 +58,20 @@ Options:
 '''.strip() + "\n")
 
         return -1
+
+def ranbal(cand=4, ball=3, elec=1, seed=None):
+    if ball < 2:
+        exit("number of ballots must at minimum 2")
+    if cand < 3:
+        exit("number of candidates must at minimum 3")
+    if elec < 1:
+        exit("number of elections must at minimum 1")
+    for e in range(elec):
+        cand_temp = list(string.ascii_uppercase + string.ascii_lowercase)
+        cand_names = cand_temp[0:cand]
+        print(cand_names)
+        ball_init = [[0] * cand for i in range(ball)]
+        for e in ball_init:
+            print (e)
+
+ranbal(cand=3, ball=4, elec=1)
