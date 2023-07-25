5bcb12ae41f159b31991575e66e7350cd82c7309
Adam Masiarek <masiarek@gmail.com>
2023-07-24 22:58:11 -0400

    ver 0.0.3

--- ranbal/__init__.py
+++ ranbal/__init__.py
@@ -32,7 +32,8 @@ __all__ = [
 class UsageException(Exception):
     def __init__(self, s=''):
         super().__init__(s)
-
+_DEFAULT_SEATS = 1
+_DEFAULT_MAXIMUM_SCORE = 5
 def main_with_usage(argv, print=builtins.print):
     try:
         main(argv, print=print)
@@ -59,19 +60,39 @@ Options:
 
         return -1
 
-def ranbal(cand=4, ball=3, elec=1, seed=None):
+def ranbal(cand=4, ball=3, elec=1, seed=None, sparsity='medium', max=_DEFAULT_MAXIMUM_SCORE):
     if ball < 2:
-        exit("number of ballots must at minimum 2")
+        exit("number of ballots must minimum 2")
     if cand < 3:
         exit("number of candidates must at minimum 3")
     if elec < 1:
         exit("number of elections must at minimum 1")
+    random.seed(seed)
     for e in range(elec):
         cand_temp = list(string.ascii_uppercase + string.ascii_lowercase)
         cand_names = cand_temp[0:cand]
-        print(cand_names)
+        candidates = ", ".join(cand_names)
+        print(candidates)
         ball_init = [[0] * cand for i in range(ball)]
+        fav_index_1 = random.randint(0, cand - 1)
+        fav_index_2 = random.randint(0,cand - 1)
+        print(fav_index_1, fav_index_2)
         for e in ball_init:
-            print (e)
+            if sparsity == 'high': # each row should have at least one/two scores
+                if cand <= 4:
+                    index1 = random.randint(0,cand - 1)
+                    e[index1] = random.randint(4, max)
+                    if (random.randint(0, 99) % 3) == 0:
+                        e[fav_index_1] = random.randint(2, max)
+                else:
+                    e[fav_index_1] = random.randint(4, max)
+                    index1 = random.randint(0, cand - 1)
+                    e[index1] = random.randint(1, max)
+                    index2 = random.randint(0, cand - 1)
+                    e[index2] = random.randint(5, max)
+                    if (random.randint(0, 99)) % 4 == 0:
+                        e[fav_index_1] = random.randint(2, max)
+                        e[fav_index_2] = random.randint(3, max)
+            print(e)
 
-ranbal(cand=3, ball=4, elec=1)
+ranbal(cand=4, ball=9, elec=1, sparsity='high', max=_DEFAULT_MAXIMUM_SCORE)
